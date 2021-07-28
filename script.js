46fededcccd649be33b284bf6165a8a4b0e02465
Henriette Dusabimana <hdusabim@gmail.com>
2021-07-28 12:07:44 +0200

    changed the UI colors

--- script.js
+++ script.js
@@ -61,53 +61,65 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-
-const displayMovements = function(movements) {
+const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
   movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit':'withdrawal'
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
     <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
       <div class="movements__value">${mov}€</div>
     </div>
-    `
-    
+    `;
+
     containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
 
-  })
-}
-displayMovements(account1.movements)
-
-const calcDisplayBalance = function(movements) {
-  const balance = movements.reduce((accumulator, curr) => accumulator + curr, 0);
-  labelBalance.textContent = `${balance}€`
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce(
+    (accumulator, curr) => accumulator + curr,
+    0
+  );
+  labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function(movements) {
-  const incomes = movements.filter(mov => mov > 0).reduce((accumulator, mov) => accumulator + mov, 0);
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((accumulator, mov) => accumulator + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements.filter(mov => mov < 0).reduce((accumulator, mov) => accumulator + mov, 0);
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((accumulator, mov) => accumulator + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements.filter(mov => mov > 0).map(deposit => deposit * 1.2/100).filter((int, i, arr) => {
-    
-  }).reduce((accumulator, int) => accumulator + int, 0);
-  labelSumInterest.textContent = `${interest}€`
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {})
+    .reduce((accumulator, int) => accumulator + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
 calcDisplaySummary(account1.movements);
 
-const createUsername = function(accts) {
-  accts.forEach(function(acc) {
-    acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('');
-  })
-}
+const createUsername = function (accts) {
+  accts.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
 
 createUsername(accounts);
-
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -136,7 +148,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //   console.log(`${key} : ${value}`);
 // });
 
-const deposits = movements.filter(function(mov, i, arr) {
+const deposits = movements.filter(function (mov, i, arr) {
   return mov > 0;
 });
 
@@ -148,11 +160,9 @@ for (const mov of movements) if (mov > 0) depositsFor.push(mov);
 
 // console.log(depositsFor);
 
-
 // FILTER METHOD
 const withdrawls = movements.filter(mov => mov < 0);
 // console.log(withdrawls);
-
 
 // REDUCE METHOD
 const balance = movements.reduce((accumulator, curr) => accumulator + curr, 0);
@@ -166,16 +176,26 @@ for (const mov of movements) balance2 += mov;
 // Maximum value
 
 const max = movements.reduce((accumulator, mov) => {
-  if (accumulator > mov)
-    return accumulator;
-  else
-    return mov;
+  if (accumulator > mov) return accumulator;
+  else return mov;
 }, movements[0]);
 // console.log(max);
 
 const eurToUsd = 1.1;
 
 // PIPELINE
-const totalDepositsUSD = movements.filter(mov => mov > 0).map(mov => mov * eurToUsd).reduce((accumulator, mov) => accumulator + mov, 0);
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((accumulator, mov) => accumulator + mov, 0);
 console.log(totalDepositsUSD);
 
+// The find Method
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Honore Manzi');
+console.log(account);
